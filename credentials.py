036a3bf47f839467da71355ec7dc9559bc0daa3f
prsaurabh2024 <164406147+prsaurabh2024@users.noreply.github.com>
2025-02-14 14:22:37 -0500

    Initial Commit

---
+++ credentials.py
@@ -0,0 +1,8 @@
+from typing import List, Dict
+from pathlib import Path
+
+
+credentials_store: Dict[str, Dict[str, str]] = {
+    "LinkedIn.com": {"username": "jtebow84@gmail.com", "password": "Suresh01"},  # Pre-populated for LinkedIn
+    "LinkedIn": {"username": "jtebow84@gmail.com", "password": "Suresh01"}  # Pre-populated for LinkedIn
+}
