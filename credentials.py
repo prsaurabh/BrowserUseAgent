22f16952637f95cc1cbb0a9616de1d9ff6944767
prsaurabh2024 <164406147+prsaurabh2024@users.noreply.github.com>
2025-02-14 14:23:46 -0500

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -3,6 +3,6 @@ from pathlib import Path
 
 
 credentials_store: Dict[str, Dict[str, str]] = {
-    "LinkedIn.com": {"username": "jtebow84@gmail.com", "password": "Suresh01"},  # Pre-populated for LinkedIn
-    "LinkedIn": {"username": "jtebow84@gmail.com", "password": "Suresh01"}  # Pre-populated for LinkedIn
+    "LinkedIn.com": {"username": "", "password": ""},  # Pre-populated for LinkedIn
+    "LinkedIn": {"username": "", "password": ""}  # Pre-populated for LinkedIn
 }
